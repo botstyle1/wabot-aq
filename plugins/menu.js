2a890948d658124b8517b194f438fcd5ea79a911
botstyle1 <84095101+botstyle1@users.noreply.github.com>
2021-05-24 16:47:23 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -41,7 +41,9 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     let rtotalreg = Object.values(global.DATABASE._data.users).filter(user => user.registered == true).length
     let tags = {
       'main': 'Main',
+      'creator': 'Creator',
       'game': 'Game',
+      'rpg': 'Rpg',
       'xp': 'Exp & Limit',
       'sticker': 'Sticker',
       'kerang': 'Kerang Ajaib',
@@ -84,26 +86,20 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     }
     conn.menu = conn.menu ? conn.menu : {}
     let before = conn.menu.before || `
-╭─「 ${conn.user.name} 」
-│ Hai, %name!
-│
-│ Tersisa *%limit Limit*
-│ Level *%level (%exp / %maxexp)* [%xp4levelup lagi untuk levelup]
-│ %totalexp XP in Total
-│ 
-│ Tanggal: *%week %weton, %date*
-│ Tanggal Islam: *%dateIslamic*
-│ Waktu: *%time*
-│
-│ Uptime: *%uptime (%muptime)*
-│ Database: %rtotalreg of %totalreg
-│ Github:
-│ %github
-╰────
+┌──「 ${conn.user.name} 」
+│============================
+├ Nama : %name!
+├ Tersisa *%limit Limit*
+├ Level *%level (%exp / %maxexp)* [%xp4levelup lagi untuk levelup]
+├ %totalexp XP in Total
+├ Tanggal: *%week %weton, %date*
+├ Waktu: *%time*
+├ Uptime: *%uptime (%muptime)*
+│============================
 %readmore`
-    let header = conn.menu.header || '╭─「 %category 」'
-    let body   = conn.menu.body   || '│ • %cmd%islimit'
-    let footer = conn.menu.footer || '╰────\n'
+    let header = conn.menu.header || '◪「 %category 」'
+    let body   = conn.menu.body   || '├❏  %cmd%islimit'
+    let footer = conn.menu.footer || '\n'
     let after  = conn.menu.after  || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + `\n*%npmname@^%version*\n\`\`\`\%npmdesc\`\`\``
     let _text  = before + '\n'
     for (let tag in groups) {
